bcd3d7805a204d4ebba409162e858a1dc22d5432
juasbhu28 <juasbhu@hotmail.com>
2023-01-15 20:25:08 -0500

    First commit application

---
+++ src/test/java/com/minmarket/HealtCheckJavaApi/HealtCheckJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minmarket.HealtCheckJavaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealtCheckJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
